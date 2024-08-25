e5f1220c4c59c380ad1b6ef43c42a1e8adca38b2
imkrunalkanojiya <kanojiyakrunal5@gmail.com>
2024-08-25 14:52:22 +0530

    initial

---
+++ src/providers/sources/zoechip/index.ts
@@ -0,0 +1,13 @@
+import { flags } from '@/entrypoint/utils/targets';
+import { makeSourcerer } from '@/providers/base';
+import { scrapeMovie } from '@/providers/sources/zoechip/scrape-movie';
+import { scrapeShow } from '@/providers/sources/zoechip/scrape-show';
+
+export const zoechipScraper = makeSourcerer({
+  id: 'zoechip',
+  name: 'ZoeChip',
+  rank: 200,
+  flags: [flags.CORS_ALLOWED],
+  scrapeMovie,
+  scrapeShow,
+});
